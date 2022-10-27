898d3dea29b5cf733084f59f766b49835c1bce0e
Collin <collin@Zipcoders-MacBook-Pro.local>
2022-10-27 13:26:18 -0400

    Created code to greet Alice and Bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,25 @@
-/**
- * Created by iyasuwatts on 10/17/17.
- */
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
 
+    public String name = "";
+    Scanner scan = new Scanner(System.in);
+    //Receive name input from user
+    // If: statement where if the name entered by user is "Alice" or "Bob", execute a greeting to user.
+    //Else: state that the user is not recognized, do not execute greeting
+    public String greeting() {
+        System.out.println("Please enter your name");
+        name = scan.next();
+        if(name == "Alice"||"Bob"){
+
+            System.out.println("Hello " + name + "!")
+        } else {
+            System.out.println("Sorry, the name you entered is not recognized. Please try again.");
+
+
+            }
+        }
     }
 }
